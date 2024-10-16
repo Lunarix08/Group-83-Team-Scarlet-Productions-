82702c7b7bec767864e7d84bc8f66986530ce861
LunarBlade08 <130893361+LunarBlade08@users.noreply.github.com>
2024-10-16 22:30:30 +0800

    Add files via upload

--- get_products.php
+++ get_products.php
@@ -19,16 +19,12 @@ if (isset($_GET['subcategory'])) {
     $sql = "SELECT * FROM products";
     $stmt = $conn->prepare($sql);
 }
-
 $stmt->execute();
 $result = $stmt->get_result();
-
-$products = array();
+$products = [];
 while ($row = $result->fetch_assoc()) {
     $products[] = $row;
 }
-
 echo json_encode($products);
-
 $conn->close();
 ?>
